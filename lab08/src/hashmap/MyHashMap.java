bf20566ed297a1c5792345c921d7450d6151b68a
MaTTthewLIU <123051911+MaTTthewLIU@users.noreply.github.com>
2024-01-06 00:48:18 -0800

    Finished

---
+++ lab08/src/hashmap/MyHashMap.java
@@ -0,0 +1,201 @@
+package hashmap;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Set;
+
+/**
+ *  A hash table-backed Map implementation. Provides amortized constant time
+ *  access to elements via get(), remove(), and put() in the best case.
+ *
+ *  Assumes null keys will never be inserted, and does not resize down upon remove().
+ *  @author YOUR NAME HERE
+ */
+public class MyHashMap<K, V> implements Map61B<K, V> {
+
+
+    /**
+     * Protected helper class to store key/value pairs
+     * The protected qualifier allows subclass access
+     */
+    protected class Node {
+        K key;
+        V value;
+
+        Node(K k, V v) {
+            key = k;
+            value = v;
+        }
+    }
+
+    /* Instance Variables */
+    private Collection<Node>[] buckets;
+    // You should probably define some more!
+    private int size = 0;
+    private int TableSize;
+    private double InitLoadFactor = 0.75;
+    private int InitCapacity = 16;
+
+
+    /** Constructors */
+    public MyHashMap() {
+        this.buckets = createTable(InitCapacity);
+        this.TableSize = InitCapacity;
+    }
+
+
+    public MyHashMap(int initialCapacity) {
+        this.buckets = createTable(initialCapacity);
+        this.TableSize = initialCapacity;
+    }
+
+    /**
+     * MyHashMap constructor that creates a backing array of initialCapacity.
+     * The load factor (# items / # buckets) should always be <= loadFactor
+     *
+     * @param initialCapacity initial size of backing array
+     * @param loadFactor maximum load factor
+     */
+    public MyHashMap(int initialCapacity, double loadFactor) {
+        this.buckets = createTable(initialCapacity);
+        this.InitLoadFactor = loadFactor;
+        this.TableSize = initialCapacity;
+
+    }
+
+    /**
+     * Returns a new node to be placed in a hash table bucket
+     */
+    private Node createNode(K key, V value) {
+        return new Node(key, value);
+    }
+
+    /**
+     * Returns a data structure to be a hash table bucket
+     *
+     * The only requirements of a hash table bucket are that we can:
+     *  1. Insert items (`add` method)
+     *  2. Remove items (`remove` method)
+     *  3. Iterate through items (`iterator` method)
+     *
+     * Each of these methods is supported by java.util.Collection,
+     * Most data structures in Java inherit from Collection, so we
+     * can use almost any data structure as our buckets.
+     *
+     * Override this method to use different data structures as
+     * the underlying bucket type
+     *
+     * BE SURE TO CALL THIS FACTORY METHOD INSTEAD OF CREATING YOUR
+     * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
+     */
+    protected Collection<Node> createBucket() {
+        return new LinkedList<>();
+    }
+
+    /**
+     * Returns a table to back our hash table. As per the comment
+     * above, this table can be an array of Collection objects
+     *
+     * BE SURE TO CALL THIS FACTORY METHOD WHEN CREATING A TABLE SO
+     * THAT ALL BUCKET TYPES ARE OF JAVA.UTIL.COLLECTION
+     *
+     * @param tableSize the size of the table to create
+     */
+    private Collection<Node>[] createTable(int tableSize) {
+        return (Collection<Node>[]) new Collection[tableSize];
+    }
+
+    @Override
+    public void put(K key, V value) {
+        int index = Math.floorMod(key.hashCode(), InitCapacity);
+        if (buckets[index] == null) {
+            buckets[index] = createBucket();
+        } else {
+            for (Node node: buckets[index]) {
+                if (node.key.equals(key)) {
+                    node.value = value;
+                    return;
+                }
+            }
+        }
+        buckets[index].add(createNode(key, value));
+        size++;
+        if ((double) size / InitCapacity >= InitLoadFactor) {
+            resize(2 * InitCapacity);
+        }
+    }
+    public void resize(int TableSize) {
+        Collection<Node>[] copy = createTable(TableSize);
+        for (Collection<Node> bucket: buckets) {
+            if (bucket == null) {
+                continue;
+            }
+            for (Node n: bucket) {
+                int Idx = Math.floorMod(n.key.hashCode(), TableSize);
+                if (copy[Idx] == null) {
+                    copy[Idx] = createBucket();
+                }
+                copy[Idx].add(createNode(n.key, n.value));
+            }
+        }
+        InitCapacity = TableSize;
+        buckets = copy;
+    }
+
+
+
+    @Override
+    public V get(K key) {
+        int index = Math.floorMod(key.hashCode(), InitCapacity);
+        if (buckets[index] != null) {
+            for (Node node: buckets[index]) {
+                if (node.key.equals(key)) {
+                    return node.value;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        int index = Math.floorMod(key.hashCode(), InitCapacity);
+        if (buckets[index] != null) {
+            for (Node node: buckets[index]) {
+                if (node.key.equals(key)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void clear() {
+        this.size = 0;
+        this.buckets = createTable(InitCapacity);
+        this.TableSize = InitCapacity;
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+
+}
